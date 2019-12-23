7957ae3836c693d35974c1af8a0883c2ab27780f
WMXPY <wm@mengw.io>
2019-12-23 11:42:16 -0600

    init activiery

---
+++ src/activity.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace HomingPigeon
+ * @description Activity
+ */
+
+export class Activity {
+
+    public static create() {
+
+        return new Activity();
+    }
+
+    private constructor() {
+
+    }
+}
