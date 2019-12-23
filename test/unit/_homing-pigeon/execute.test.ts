d8a7b42d0b6879b38cb1bd8cf3b3538a7ad073b0
WMXPY <wm@mengw.io>
2019-12-23 14:11:08 -0600

    implement execute unit tests

--- test/unit/_homing-pigeon/execute.test.ts
+++ test/unit/_homing-pigeon/execute.test.ts
@@ -18,20 +18,169 @@ describe('Given {HomingPigeon} Class - [Execute] Function', (): void => {
     it('should be able to execute - Happy Path', async (): Promise<void> => {
 
         const moduleName: string = chance.string();
+        let count: number = 0;
 
         const instance: HomingPigeon = HomingPigeon.create();
         instance.module({
             name: moduleName,
             validate: () => true,
-            execute: async () => true,
+            execute: async () => {
+                count++;
+                return true;
+            },
         });
 
         const executeResult: ExecuteResult = await instance.execute(createMockActivity(chance, moduleName));
 
+        expect(count).to.be.equal(1);
         expect(executeResult).to.be.deep.equal({
             proceed: true,
             missed: [],
             succeed: [moduleName],
+            validateFailed: [],
+            executeFailed: [],
+            errors: {},
+        } as ExecuteResult);
+    });
+
+    it('should be able to execute - Validate Failed Path', async (): Promise<void> => {
+
+        const moduleName: string = chance.string();
+        let count: number = 0;
+
+        const instance: HomingPigeon = HomingPigeon.create();
+        instance.module({
+            name: moduleName,
+            validate: () => false,
+            execute: async () => {
+                count++;
+                return true;
+            },
+        });
+
+        const executeResult: ExecuteResult = await instance.execute(createMockActivity(chance, moduleName));
+
+        expect(count).to.be.equal(0);
+        expect(executeResult).to.be.deep.equal({
+            proceed: true,
+            missed: [],
+            succeed: [],
+            validateFailed: [moduleName],
+            executeFailed: [],
+            errors: {},
+        } as ExecuteResult);
+    });
+
+    it('should be able to execute - Required Validate Failed Path', async (): Promise<void> => {
+
+        const moduleName: string = chance.string();
+        let count: number = 0;
+
+        const instance: HomingPigeon = HomingPigeon.create();
+        instance.module({
+            name: moduleName,
+            required: true,
+            validate: () => false,
+            execute: async () => {
+                count++;
+                return true;
+            },
+        });
+
+        const executeResult: ExecuteResult = await instance.execute(createMockActivity(chance, moduleName));
+
+        expect(count).to.be.equal(0);
+        expect(executeResult).to.be.deep.equal({
+            proceed: false,
+            missed: [],
+            succeed: [],
+            validateFailed: [moduleName],
+            executeFailed: [],
+            errors: {},
+        } as ExecuteResult);
+    });
+
+    it('should be able to execute - Execute Failed Path', async (): Promise<void> => {
+
+        const moduleName: string = chance.string();
+        let count: number = 0;
+
+        const instance: HomingPigeon = HomingPigeon.create();
+        instance.module({
+            name: moduleName,
+            validate: () => true,
+            execute: async () => {
+                count++;
+                return false;
+            },
+        });
+
+        const executeResult: ExecuteResult = await instance.execute(createMockActivity(chance, moduleName));
+
+        expect(count).to.be.equal(1);
+        expect(executeResult).to.be.deep.equal({
+            proceed: true,
+            missed: [],
+            succeed: [],
+            validateFailed: [],
+            executeFailed: [moduleName],
+            errors: {},
+        } as ExecuteResult);
+    });
+
+    it('should be able to execute - Error Path', async (): Promise<void> => {
+
+        const moduleName: string = chance.string();
+        const message: string = chance.string();
+        let count: number = 0;
+
+        const instance: HomingPigeon = HomingPigeon.create();
+        instance.module({
+            name: moduleName,
+            validate: () => true,
+            execute: async () => {
+                count++;
+                throw message;
+            },
+        });
+
+        const executeResult: ExecuteResult = await instance.execute(createMockActivity(chance, moduleName));
+
+        expect(count).to.be.equal(1);
+        expect(executeResult).to.be.deep.equal({
+            proceed: true,
+            missed: [],
+            succeed: [],
+            validateFailed: [],
+            executeFailed: [moduleName],
+            errors: {
+                [moduleName]: message,
+            },
+        } as ExecuteResult);
+    });
+
+    it('should be able to execute - Missed Path', async (): Promise<void> => {
+
+        const moduleName: string = chance.string();
+        let count: number = 0;
+
+        const instance: HomingPigeon = HomingPigeon.create();
+        instance.module({
+            name: chance.string(),
+            validate: () => true,
+            execute: async () => {
+                count++;
+                return true;
+            },
+        });
+
+        const executeResult: ExecuteResult = await instance.execute(createMockActivity(chance, moduleName));
+
+        expect(count).to.be.equal(0);
+        expect(executeResult).to.be.deep.equal({
+            proceed: false,
+            missed: [moduleName],
+            succeed: [],
             validateFailed: [],
             executeFailed: [],
             errors: {},
